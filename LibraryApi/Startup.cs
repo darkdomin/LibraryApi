cad94feb82f7ff53efe7cbd3257e938fd90d6bff
darkdomin <darkdomin@wp.pl>
2021-10-26 12:25:28 +0200

    Add Cors

--- LibraryApi/Startup.cs
+++ LibraryApi/Startup.cs
@@ -74,12 +74,21 @@ namespace LibraryApi
             services.AddScoped<IUserContextService, UserContextService>();
             services.AddHttpContextAccessor();
             services.AddSwaggerGen();
+            services.AddCors(options=>
+            {
+                options.AddPolicy("FrontEndClient", builder =>
 
+                builder.AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .WithOrigins(Configuration["AllowedOrigins"])
+                ); 
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, LibrarySeeder librarySeeder)
         {
+            app.UseCors("FrontEndClient");
             librarySeeder.Seed();
             if (env.IsDevelopment())
             {
